537d0b189d3aff87f7a53498b65326f8eb761e4a
Sumitprajapatintv <sumit963605@gmail.com>
2026-03-12 15:33:24 +0530

    Doubly Linked List

---
+++ LinkedList/doubly.js
@@ -0,0 +1,88 @@
+class Node{
+   constructor(data,next=null,prev=null)
+   {
+    this.data=data;
+    this.next=next;
+    this.prev=prev
+   } 
+}
+
+function convertArray2DoblyList(arr)
+{
+    let head=new Node(arr[0])
+    let back=head;
+    for(let i=1;i<arr.length;i++)
+    {
+        let temp=new Node(arr[i])
+        back.next=temp;
+        temp.prev=back
+        back=temp
+    }
+    return head
+}
+
+function insertAtHead(head,value)
+{
+   let newNode=new Node(value,head,null)
+   head.prev=newNode
+   return newNode;    
+}
+
+function insertBeforeTail(head,value)
+{
+    if(head.next==null)
+    {
+        return insertAtHead(head,value)
+    }
+    let back=head;
+    while(back.next!=null)
+    {
+        back=back.next
+    }
+  
+    let prevNode=back.prev
+    let newNode=new Node(value,back,prevNode)
+    prevNode.next=newNode
+    back.prev=newNode
+    return head
+}
+
+function insertBeforeKthPostion(head,value,k)
+{
+    let count=1;
+    let temp=head;
+    while(count!=k)
+    {
+        if(count==k)
+        {
+            break;
+        }
+        count++;
+        temp=temp.next
+    }
+    let prevNode=temp.prev
+    let newNode=new Node(value,temp,prevNode)
+    prevNode.next=newNode
+    temp.prev=newNode
+    return head
+}
+
+function printList(h)
+{
+    let temp=h;
+    while(temp)
+    {
+        console.log(temp.data);
+        temp=temp.next
+    }
+}
+
+let h=convertArray2DoblyList([1]    )
+h=insertAtHead(h,22)
+h=insertAtHead(h,23)
+h=insertAtHead(h,24)
+h=insertBeforeTail(h,44)
+h=insertBeforeTail(h,4)
+
+h=insertBeforeKthPostion(h,99,2)
+printList(h)
